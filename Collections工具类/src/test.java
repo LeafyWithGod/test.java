92d68845b7e145c0103ecc67e1d5c57f255f70df
LeafyWithGod <98461824+LeafyWithGod@users.noreply.github.com>
2022-06-06 12:47:46 +0800

    Add files via upload

---
+++ Collections工具类/src/test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+        List<Integer> li=new ArrayList<>();
+        li.add(129);
+        li.add(28);
+        li.add(78);
+
+        //Collections.sort(li);//排序
+        //Collections.reverse(li);//反转
+        //Collections.shuffle(li);//打乱
+        System.out.println(li);
+    }
+}
